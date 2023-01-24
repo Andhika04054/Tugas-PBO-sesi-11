97e3fa1960298dc0880cf7a5bef88d17237eb912
Andhika04054 <121345916+Andhika04054@users.noreply.github.com>
2023-01-24 23:12:56 +0700

    Add files via upload

---
+++ percobaan1/ExceptionError.java
@@ -0,0 +1,12 @@
+
+package percobaan.pkg1;
+/**
+ *
+ * @author Axioo
+ */
+public class ExceptionError {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[5]=100;
+    }
+}
